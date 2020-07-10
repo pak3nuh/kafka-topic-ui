1b4d78d5fdd0afd58257f4a8a503c1e2591939fc
Nuno Caro <Nuno.Caro@criticaltechworks.com>
2020-07-10 11:37:20 +0100

    Small styling changes

--- src/main/kotlin/pt/pak3nuh/kafka/ui/view/TopicDetailView.kt
+++ src/main/kotlin/pt/pak3nuh/kafka/ui/view/TopicDetailView.kt
@@ -2,6 +2,7 @@ package pt.pak3nuh.kafka.ui.view
 
 import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.ObservableList
+import javafx.geometry.Pos
 import javafx.scene.Parent
 import javafx.scene.control.ToggleGroup
 import javafx.scene.layout.VBox
@@ -41,9 +42,13 @@ class TopicDetailView : CoroutineView("Topic Detail") {
     )))
 
     override val root: Parent = borderpane {
+        prefWidth = 500.0
+        prefHeight = 500.0
         title = topic.name
         // start pause seek settings assignment
         top = hbox {
+            spacing = 10.0
+            alignment = Pos.CENTER
             val group = ToggleGroup()
             radiobutton("Earliest", group) {
                 action {
